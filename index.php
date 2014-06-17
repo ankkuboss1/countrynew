cb7f0ee392b5521edc5068e5966258b7c8d1c1bb
ankkuboss1 <ankitpatidar@hostnsoft.com>
2014-06-17 11:54:26 +0530

    my fisrt commit
    let me do it

---
+++ index.php
@@ -0,0 +1,20 @@
+<script type="text/javascript" src="js/country.js"></script>
+Select Country (with states):   <select id="country" name ="country"></select>
+Select State: <select name ="state" id ="state"></select>
+ <script language="javascript">
+populateCountries("country", "state");
+
+
+
+
+
+
+
+
+
+
+
+//jhhkjhj
+//dear again
+//heelolo dear
+ </script>
